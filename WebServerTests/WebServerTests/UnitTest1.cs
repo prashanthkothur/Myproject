441aa3716ee20ae8e2abba1750614b5554a1a8b2
Prashanth Kothur <pkothur@salesforce.com>
2017-08-07 00:05:07 -0400

    Adding Tests

---
+++ WebServerTests/WebServerTests/UnitTest1.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using WebServer;
+
+namespace WebServerTests
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod_FetchTwitterFeed()
+        {
+            var searchText = string.Empty;
+            var _twitterFeedReader = new TwitterFeedReader();
+            var feedText = _twitterFeedReader.FetchTwitterFeed(searchText);
+            Assert.IsTrue(feedText.Length > 0);
+        }
+
+        [TestMethod]
+        public void TestMethod_FetchTwitterFeed_With_SearchText()
+        {
+            var searchText = "OR @salesforce";
+            var _twitterFeedReader = new TwitterFeedReader();
+            var feedText = _twitterFeedReader.FetchTwitterFeed(searchText);
+            Assert.IsTrue(feedText.Length > 0);
+        }
+    }
+}
